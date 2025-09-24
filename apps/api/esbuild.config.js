14769be5510b5b1df86a819973cb361ba545562e
Pachi <pachibadilla@gmail.com>
2025-09-24 17:15:45 +0200

    :green_heart: Esbuild again

--- apps/api/esbuild.config.js
+++ apps/api/esbuild.config.js
@@ -1,11 +1,19 @@
 import { build } from 'esbuild';
+import { dependencies } from './package.json' assert { type: 'json' };
+
+const external = [
+  ...Object.keys(dependencies || {}), // all npm deps
+  'node:events', // explicitly exclude node core module
+];
 
 build({
   entryPoints: ['src/index.ts'],
   bundle: true,
   platform: 'node',
   format: 'esm',
+  target: 'node22',
   outfile: 'build/index.js',
   sourcemap: true,
   tsconfig: 'tsconfig.json',
+  external, // <-- critical
 }).catch(() => process.exit(1));
